8a72d60a4213b979acec89366e9005a9a535fa72
CattenLinger <cattenlinger@outlook.com>
2022-07-22 23:06:57 +0800

    Save

--- jam-networkd-ip-json/src/main/kotlin/com/shinonometn/jam/networkd/ipjson/IpJsonCommandService.kt
+++ jam-networkd-ip-json/src/main/kotlin/com/shinonometn/jam/networkd/ipjson/IpJsonCommandService.kt
@@ -9,19 +9,19 @@ import org.apache.commons.lang3.SystemUtils
 class IpJsonCommandService : NetworkInterfaceProvider {
 
     override fun listInterfaces(callback: (Result<List<NetworkInterface>>) -> Unit) {
-        shell(*ip("link")).execute {
+        shell(ip("link")).execute {
             if(it.isFailure) callback(Result.failure(it.exceptionOrNull()!!))
             else callback(Result.success(it.getOrThrow().outputAsJson().map(IpCommandInterface::fromJson)))
         }
     }
 
-    private fun ip(vararg args : String) : Array<String> {
-        return arrayOf("ip", "-j") + args
+    private fun ip(vararg args : String) : String {
+        return (arrayOf("ip", "-j") + args).joinToString(" ")
     }
 
     override fun isSupported(): BaseService.CompatibilityReport {
         if(!SystemUtils.IS_OS_LINUX) return BaseService.CompatibilityReport.notSupported("Require System: Linux")
-        val executeResult = shell("command", "-v", "ip").execute().get()
+        val executeResult = shell("command -v ip").execute().get()
         if(executeResult.exitCode != 0) return BaseService.CompatibilityReport.notSupported("Require command: ip")
         return BaseService.CompatibilityReport.supported()
     }
